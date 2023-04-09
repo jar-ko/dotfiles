29d8c9803ba14aea0b3382c965f399457aef762c
Jar.Ko <jar.ko@protonmail.com>
2023-04-09 18:43:43 +0100

    updated

--- tmp/XsltGenerator.java
+++ tmp/XsltGenerator.java
@@ -1,13 +1,17 @@
 package org.example;
 
-import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
 import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.parser.OpenAPIV3Parser;
 import io.swagger.v3.parser.core.models.ParseOptions;
+import org.apache.commons.lang3.StringUtils;
 
-import java.util.List;
+//    implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.14.2'
+//    implementation 'com.fasterxml.jackson.core:jackson-databind:2.14.2'
+//    implementation 'io.swagger.parser.v3:swagger-parser:2.1.12'
+//    implementation 'org.apache.commons:commons-lang3:3.12.0'
 
 public class XsltGenerator {
     private static final String SWAGGER_YAML_PATH = "/swagger.yaml";
@@ -15,13 +19,13 @@ public class XsltGenerator {
     private static final Method METHOD = Method.POST;
     private static final RequestResponse REQUEST_RESPONSE = RequestResponse.REQUEST;
     private static final String MEDIA_TYPE = "application/json";
-    private static final List<String> SUCCESSFUL_RESPONSES_CODES = List.of("200");
+    private static final String SUCCESSFUL_RESPONSE_CODE = "200";
 
     public static void main(String[] args) {
         ParseOptions parseOptions = new ParseOptions();
         parseOptions.setResolveFully(true);
 
-        PathItem pathItem = new OpenAPIParser()
+        PathItem pathItem = new OpenAPIV3Parser()
                 .readLocation(SWAGGER_YAML_PATH, null, parseOptions)
                 .getOpenAPI()
                 .getPaths()
@@ -36,21 +40,27 @@ public class XsltGenerator {
                 .getValue()
                 .getSchema();
 
-        parseSchema(schema, "jsonObject");
+        parseSchema(schema, "jsonObject", 0);
     }
 
-    private static void parseSchema(Schema<?> schema, String path) {
-        System.out.println(path + " " + schema.getType());
+    private static void parseSchema(Schema<?> schema, String path, int prepend) {
+        System.out.println(StringUtils.leftPad("", prepend, " ") + path + " " + schema.getType());
         if ("object".equalsIgnoreCase(schema.getType())) {
             schema
                     .getProperties()
-                    .forEach((key, value) -> parseSchema(value, path + "/" + key));
+                    .forEach((key, value) -> parseSchema(value, path + "/" + key, prepend + 2));
         } else if ("array".equalsIgnoreCase(schema.getType())) {
             if ("object".equalsIgnoreCase(schema.getItems().getType())) {
-                parseSchema(schema.getItems(), path);
+                parseSchema(schema.getItems(), path, prepend + 2);
             } else {
-                System.out.println(path + " " + schema.getItems().getType());
+                System.out.println(StringUtils.leftPad("", prepend, " ") + path + " " + schema.getItems().getType());
             }
+        } else if (schema.getOneOf() != null) {
+            System.out.println(StringUtils.leftPad("", prepend, " ") + "# oneOf start");
+            schema
+                    .getOneOf()
+                    .forEach(it -> parseSchema(it, path, prepend + 2));
+            System.out.println(StringUtils.leftPad("", prepend, " ") + "# oneOf end");
         }
     }
 
@@ -65,7 +75,7 @@ public class XsltGenerator {
                 .getResponses()
                 .entrySet()
                 .stream()
-                .filter(it -> SUCCESSFUL_RESPONSES_CODES.contains(it.getKey()))
+                .filter(it -> SUCCESSFUL_RESPONSE_CODE.equalsIgnoreCase(it.getKey()))
                 .findFirst()
                 .orElseThrow()
                 .getValue()
